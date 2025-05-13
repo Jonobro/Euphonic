733670fa9412226b1814cd679e4ad24fa759ed55
jonobro <jonobro@gmail.com>
2025-05-13 13:04:57 -0500

    Removed analytics page from app

--- spotify_project/spotify_auth/urls.py
+++ spotify_project/spotify_auth/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('callback/', views.spotify_callback, name='spotify_callback'),
     path('logout/', views.logout_view, name='logout'),
     path('chat/', views.chat_view, name='chat'),
-    path('analytics/', views.analytics_view, name='analytics'),
 ]
