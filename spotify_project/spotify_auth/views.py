43a02221ae33e2c8cf72782ed8a4b98d7b1a0b0b
jonobro <jonobro@gmail.com>
2025-06-25 23:05:24 -0500

    Minor tweak to LLM instructions regarding number of songs

--- spotify_project/spotify_auth/views.py
+++ spotify_project/spotify_auth/views.py
@@ -95,7 +95,7 @@ SYSTEM_INSTRUCTION = """\
         *   Aim for playlists of approximately 50 songs.
         *   Adjust length based on the specificity of the request (e.g., using only user's saved songs can result in fewer songs) or if the user specifies a desired length.
         *   If more than 50 songs closely match criteria, include them, but never exceed 250 songs per playlist.
-        *   Don't ever mention the number of songs (even when repeating the user's prompt back to them).
+        *   Don't ever mention the number of songs in the playlists you create.
     6.  **Song Descriptions:** Include descriptions for songs in a playlist only if contextually warranted and beneficial to the user's request. Generally, omit them.
     7.  **Playlist Naming:** When generating a playlist, you must give it a name. Include the playlist name on its own line before the list of songs, enclosing it with + signs in this exact format: +++++Playlist Name+++++
 
