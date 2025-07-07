0078bb3aae35d9103081cd9e844e1c9657c9f2be
jonobro <jonobro@gmail.com>
2025-07-07 12:40:06 -0500

    Fixed issue where initial prompt would display on reload

--- spotify_project/spotify_auth/views.py
+++ spotify_project/spotify_auth/views.py
@@ -1257,6 +1257,7 @@ def _process_chat_message_thread(session_data, user_message, task_id):
 
                 final_history_for_session = [item for item in serializable_history]
                 final_history_for_session[-1]['parts'] = [{'text': ai_response_text}]
+                final_history_for_session = final_history_for_session[1:]
                 mock_request.session['final_analysis_chat_history'] = final_history_for_session
             
             result = {
@@ -1544,6 +1545,7 @@ def _process_chat_message_thread(session_data, user_message, task_id):
 
             final_history_for_session = [item for item in serializable_history]
             final_history_for_session[-1]['parts'] = [{'text': processed_ai_response_text}]
+            final_history_for_session = final_history_for_session[1:]
             mock_request.session[final_chat_history_placeholder] = final_history_for_session
         
         result = {
