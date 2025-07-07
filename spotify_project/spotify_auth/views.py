ee41bf9c2eedbfde75f01e75efc3ad24943872f9
jonobro <jonobro@gmail.com>
2025-07-07 13:30:25 -0500

    Fixed error with initial prompt being displayed for new songs chat

--- spotify_project/spotify_auth/views.py
+++ spotify_project/spotify_auth/views.py
@@ -1063,7 +1063,8 @@ I've talked too much – let's get started! What can I do for you?"""
             history_list.append({'role': 'user', 'parts': [{'text': initial_prompt}]})
             history_list.append({'role': 'model', 'parts': [{'text': initial_response}]})
             request.session['new_songs_chat_history'] = history_list
-            request.session['final_new_songs_chat_history'] = history_list
+            final_history_list = [{'role': 'model', 'parts': [{'text': initial_response}]}]
+            request.session['final_new_songs_chat_history'] = final_history_list
             request.session.modified = True
             return JsonResponse({'first_ai_message': [initial_response]})
 
@@ -1259,7 +1260,7 @@ def _process_chat_message_thread(session_data, user_message, task_id):
                 final_history_for_session[-1]['parts'] = [{'text': ai_response_text}]
                 final_history_for_session = final_history_for_session[1:]
                 if mock_request.session['library_size_message']:
-                    final_history_for_session.insert(2, {'role': 'model', 'parts': [{'text': mock_request.session['library_size_message']}]})
+                    final_history_for_session.insert(1, {'role': 'model', 'parts': [{'text': mock_request.session['library_size_message']}]})
                 mock_request.session['final_analysis_chat_history'] = final_history_for_session
             
             result = {
@@ -1549,7 +1550,7 @@ def _process_chat_message_thread(session_data, user_message, task_id):
             final_history_for_session[-1]['parts'] = [{'text': processed_ai_response_text}]
             final_history_for_session = final_history_for_session[1:]
             if chat_mode == 'saved_songs' and mock_request.session['library_size_message']:
-                final_history_for_session.insert(2, {'role': 'model', 'parts': [{'text': mock_request.session['library_size_message']}]})
+                final_history_for_session.insert(1, {'role': 'model', 'parts': [{'text': mock_request.session['library_size_message']}]})
             mock_request.session[final_chat_history_placeholder] = final_history_for_session
         
         result = {
