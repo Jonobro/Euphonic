6ee7b00fb594e127c4661db357eda0c0b9319ef2
jonobro <jonobro@gmail.com>
2025-07-16 09:40:03 -0500

    Disabling chat input when buttons appear

--- spotify_project/spotify_auth/static/spotify_auth/js/playlist_handler.js
+++ spotify_project/spotify_auth/static/spotify_auth/js/playlist_handler.js
@@ -50,6 +50,7 @@ function processMessageForPlaylist(messageElement) {
         
         if (trackLinks.length > 0) {
             messageElement.classList.add('has-playlist-button');
+            toggleChatInput(true);
             
             const playlistActionsContainer = document.createElement('div');
             playlistActionsContainer.className = 'save-playlist-container';
