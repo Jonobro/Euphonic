f492b0e9234e6dbd536448013d9393a14870c574
jonobro <jonobro@gmail.com>
2025-08-13 17:40:19 -0500

    Added max_output_tokens argument to chat configs

--- spotify_project/spotify_auth/views.py
+++ spotify_project/spotify_auth/views.py
@@ -636,8 +636,11 @@ DEVELOPER MESSAGE: ANALYZE THE ABOVE LIBRARY AND PROVIDE YOUR INSIGHTS PER THE R
             tools=current_tools,
             response_modalities=["TEXT"],
             safety_settings=SAFETY_SETTINGS,
-            # thinking_config=types.ThinkingConfig(thinking_budget=-1, include_thoughts=True)
-            thinking_config=types.ThinkingConfig(thinking_budget=1024, include_thoughts=True)
+            # thinking_config=types.ThinkingConfig(thinking_budget=-1, include_thoughts=True),
+
+            # Testing custom settings
+            thinking_config=types.ThinkingConfig(thinking_budget=1024, include_thoughts=True),
+            max_output_tokens = 1024
         )
         chat = client.chats.create(
             model=MODEL_NAME,
@@ -1265,8 +1268,11 @@ def _process_chat_message_thread(session_data, user_message, task_id, chat_mode)
             tools=first_pass_tools,
             response_modalities=["TEXT"],
             safety_settings=SAFETY_SETTINGS,
-            # thinking_config=types.ThinkingConfig(thinking_budget=-1, include_thoughts=True)
-            thinking_config=types.ThinkingConfig(thinking_budget=1024, include_thoughts=True)
+            # thinking_config=types.ThinkingConfig(thinking_budget=-1, include_thoughts=True),
+
+            # Testing custom settings
+            thinking_config=types.ThinkingConfig(thinking_budget=1024, include_thoughts=True),
+            max_output_tokens = 1024
         )
         
         chat = client.chats.create(
@@ -1345,8 +1351,11 @@ def _process_chat_message_thread(session_data, user_message, task_id, chat_mode)
             formatting_chat_config = types.GenerateContentConfig(
                 system_instruction=FORMATTING_SYSTEM_INSTRUCTION,
                 safety_settings=SAFETY_SETTINGS,
-                # thinking_config=types.ThinkingConfig(thinking_budget=-1, include_thoughts=True)
-                thinking_config=types.ThinkingConfig(thinking_budget=1024, include_thoughts=True)
+                # thinking_config=types.ThinkingConfig(thinking_budget=-1, include_thoughts=True),
+
+                # Testing custom settings
+                thinking_config=types.ThinkingConfig(thinking_budget=1024, include_thoughts=True),
+                max_output_tokens = 1024
             )
 
             formatting_chat = client.chats.create(
@@ -1506,8 +1515,11 @@ def _process_chat_message_thread(session_data, user_message, task_id, chat_mode)
                 tools=feedback_pass_tools,
                 response_modalities=["TEXT"],
                 safety_settings=SAFETY_SETTINGS,
-                # thinking_config=types.ThinkingConfig(thinking_budget=-1, include_thoughts=True)
-                thinking_config=types.ThinkingConfig(thinking_budget=1024, include_thoughts=True)
+                # thinking_config=types.ThinkingConfig(thinking_budget=-1, include_thoughts=True),
+
+                # Testing custom settings
+                thinking_config=types.ThinkingConfig(thinking_budget=1024, include_thoughts=True),
+                max_output_tokens = 1024
             )
 
             feedback_chat = client.chats.create(
@@ -1616,8 +1628,11 @@ def _process_chat_message_thread(session_data, user_message, task_id, chat_mode)
                     tools=removal_pass_tools,
                     response_modalities=["TEXT"],
                     safety_settings=SAFETY_SETTINGS,
-                    # thinking_config=types.ThinkingConfig(thinking_budget=-1, include_thoughts=True)
-                    thinking_config=types.ThinkingConfig(thinking_budget=1024, include_thoughts=True)
+                    # thinking_config=types.ThinkingConfig(thinking_budget=-1, include_thoughts=True),
+
+                    # Testing custom settings
+                    thinking_config=types.ThinkingConfig(thinking_budget=1024, include_thoughts=True),
+                    max_output_tokens = 1024
                 )
 
                 removal_chat = client.chats.create(
