b445d0575800124eba0b3fedb19179e176d529ae
jonobro <jonobro@gmail.com>
2025-09-26 16:39:14 -0500

    Increasing thinking token budget to 8000 for additional testing

--- spotify_project/spotify_auth/views.py
+++ spotify_project/spotify_auth/views.py
@@ -1161,8 +1161,8 @@ def _process_chat_message_thread(session_data, user_message, task_id, chat_mode)
 
         # Can add "include_thoughts=True" to see thought summaries
         thinking_config_map = {
-            'analysis': types.ThinkingConfig(thinking_budget=4000),
-            'saved_songs': types.ThinkingConfig(thinking_budget=4000),
+            'analysis': types.ThinkingConfig(thinking_budget=8000),
+            'saved_songs': types.ThinkingConfig(thinking_budget=8000),
             'new_songs': types.ThinkingConfig(thinking_budget=-1),
         }
         thinking_config_for_mode = thinking_config_map.get(chat_mode)
